8525b32e764c4da760dfce631be6521cc70a0774
avgonzalez87 <avgonzalez87@ucatolica.edu.co>
2023-08-30 18:58:06 -0500

    Solucion principios segregacion y dependencia
    Se realiza la solucion a los ejercicios  de los principios solid

--- InversionDependencia/Factura.java
+++ InversionDependencia/Factura.java
@@ -24,3 +24,11 @@ importeTotal = (importeFactura - importeDeduccion) + importeIVA;
 
 /**1.Cumple el principio de Inversión de dependencia. Justifica la respuesta.
 2. En caso negativo, refactoriza el código par que cumpla el principio. */
+
+/**
+1.Si, cumple con el principio de dependencia de acuerdo a que Este diseño permite una mayor flexibilidad,
+de acuerdo con que si en el futuro se  necesita cambiar cómo se calcula la deducción o el IVA, 
+solo necesitaria cambiar la implementación en las clases Deduccion e Iva sin afectar la clase Factura. 
+Por lo anterior, se puede decir que este diseño respeta el
+2. No se requiere de refactorizar
+el codigo de acuerdo a que cumple con el principio   */
--- Segregacion/Contacto.java
+++ Segregacion/Contacto.java
@@ -26,3 +26,109 @@ String name, emailAddress;
 }
 Crea un programa que permita invocar al método sendEmail de la clase EmailSender con
 un objeto de la clase GmailAccount. */
+/**Solución:
+1. Las clases EmailSender y SMSSender necesitan información puntualde la clase Contacto para realizar 
+su tarea de enviar correos electrónicos y SMS respectivamente. 
+por lo tanto, la información que necesitan es la información de contacto del destinatario, 
+que está representada por las propiedades de la clase Contacto:
+
+    EmailSender: Necesita la dirección de correo electrónico del contacto para enviar un 
+    correo electrónico.
+
+    SMSSender: Necesita el número de teléfono del contacto para enviar un mensaje SMS.
+
+La información que recogen estas clases es el mensaje que se enviará al contacto.
+
+En este caso, parece que tanto EmailSender como SMSSender incumplen el principio ISP. 
+Esto se debe a que estas clases son clases de envío de mensajes y solo necesitan los 
+datos relevantes para su acción particular (dirección de correo electrónico para email 
+y número de teléfono para SMS), pero en su forma actual, estas clases también tienen
+ que implementar métodos sendEmail y sendSMS, incluso si estos métodos no son relevantes 
+ para la funcionalidad de envío de mensajes en sí
+
+2. public interface ContactInformation {
+    String getName();
+    String getAddress();
+    String getEmailAddress();
+    String getTelephone();
+}
+public class Contacto implements ContactInformation {
+    private String name, address, emailAddress, telephone;
+
+    public void setName(String n) { name = n; }
+    public String getName() { return name; }
+    public void setAddress(String a) { address = a; }
+    public String getAddress() { return address; }
+    public void setEmailAddress(String ea) { emailAddress = ea; }
+    public String getEmailAddress() { return emailAddress; }
+    public void setTelephone(String t) { telephone = t; }
+    public String getTelephone() { return telephone; }
+}
+public interface MessageSender {
+    void sendMessage(ContactInformation contact, String message);
+}
+public class EmailSender implements MessageSender {
+    public void sendMessage(ContactInformation contact, String message) {
+        // Envía un correo electrónico utilizando contact.getEmailAddress()
+    }
+}
+public class SMSSender implements MessageSender {
+    public void sendMessage(ContactInformation contact, String message) {
+        // Envía un mensaje SMS utilizando contact.getTelephone()
+    }
+}
+Se definido una interfaz ContactInformation que define los métodos necesarios para acceder a
+ la información de contacto. La clase Contacto implementa esta interfaz.
+Se creola interfaz MessageSender para definir el método sendMessage, y las clases EmailSender 
+y SMSSender implementan esta interfaz para enviar correos electrónicos y mensajes SMS.
+3. 
+
+
+public interface ContactInformation {
+    String getName();
+    String getAddress();
+    String getEmailAddress();
+    String getTelephone();
+}
+
+public class Contacto implements ContactInformation {
+    // ...
+}
+public class GmailAccount implements ContactInformation {
+    private String name, emailAddress;
+
+    // Implementa los métodos de la interfaz ContactInformation
+    public String getName() { return name; }
+    public String getAddress() { return null; }  // Puedes dejar este como null para Gmail
+    public String getEmailAddress() { return emailAddress; }
+    public String getTelephone() { return null; }  // Puedes dejar este como null para Gmail
+}
+
+public class EmailSender implements MessageSender {
+    public void sendMessage(ContactInformation contact, String message) {
+        // Envía un correo electrónico utilizando contact.getEmailAddress()
+    }
+}
+
+public class SMSSender implements MessageSender {
+    public void sendMessage(ContactInformation contact, String message) {
+        // Envía un mensaje SMS utilizando contact.getTelephone()
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        GmailAccount gmailAccount = new GmailAccount();
+        gmailAccount.setName("John Doe");
+        gmailAccount.setEmailAddress("johndoe@gmail.com");
+
+        EmailSender emailSender = new EmailSender();
+        String message = "Hola, esto es un correo de prueba.";
+        emailSender.sendMessage(gmailAccount, message);
+    }
+}
+Secrea un objeto GmailAccount, lo configura con un nombre y una dirección 
+de correo electrónico, y luego utiliza el EmailSender para enviar un correo electrónico 
+utilizando el método sendMessage. La clave aquí es que la clase GmailAccount implementa 
+la interfaz ContactInformation, lo que le permite ser utilizado en el contexto del EmailSender.
+ */
